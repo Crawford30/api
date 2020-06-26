caae9ebeaa378c93fec317a820e74499b60150c1
joel <joelcrawford30@gmail.com>
2020-06-26 12:39:06 +0300

    Relationship created

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -2,9 +2,17 @@
 
 namespace App\Model;
 
+
+use App\Model\Review;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    //A product cab have many reviews// creating relationship
+
+    public function reviews()
+    {
+    	return $this->hasMany(Review::class); 
+    }
 }
--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -9,19 +9,12 @@ use App\Model\Product;
 use Faker\Generator as Faker;
 
 $factory->define(Review::class, function (Faker $faker) {
-    return [
-        //
-
-        'product_id' => function() {
-
-        		return Product::all() -> random();
-
-        },
-
-        'customer' => $faker -> name,
-
-        'review' => $faker -> paragraph,
-
-        'star' => $faker -> numberBetween(0,5),
+     return [
+    	'product_id' => function(){
+    		return Product::all()->random();	
+    	},
+        'customer' => $faker->name,
+        'review' => $faker->paragraph,
+        'star' => $faker->numberBetween(0,5)
     ];
 });
